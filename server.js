7e98fdc1ed24810a438adfcac0dafb9bd4f4a949
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2025-02-04 00:12:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.get("/oc", async (req, res) => {
 });
 
 module.exports = app;
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
